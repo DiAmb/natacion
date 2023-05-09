da47913555e1918d5caae41f90e6264d1825f710
DiegoAmbrocio <diehgo2.229@gmail.com>
2023-05-08 18:29:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/natacion/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.natacion
+
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("usuario")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+
+            }
+            .addOnFailureListener { e ->
+            }
+    }
+}
